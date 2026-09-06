72ee96b0f3f11d5182a4d087c895f3299c4f9c5a
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-06 23:48:09 +0330

    feat: add tasks slice

---
+++ src/features/tasks/tasksSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { Task } from "../../types/task";
+
+type TasksState = {
+  tasks: Task[];
+};
+
+const initialState: TasksState = {
+  tasks: [],
+};
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {
+    setTasks: (state, action: PayloadAction<Task[]>) => {
+      state.tasks = action.payload;
+    },
+
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.tasks.push(action.payload);
+    },
+
+    updateTask: (state, action: PayloadAction<Task>) => {
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+
+      if (index !== -1) {
+        state.tasks[index] = action.payload;
+      }
+    },
+
+    deleteTask: (state, action: PayloadAction<string>) => {
+      state.tasks = state.tasks.filter(
+        (task) => task.id !== action.payload
+      );
+    },
+  },
+});
+
+export const {
+  setTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+} = tasksSlice.actions;
+
+export default tasksSlice.reducer;

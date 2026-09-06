4fb75c1396a0083e79a77627a4882eeaea22dbff
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-06 23:43:22 +0330

    feat: add projects slice

---
+++ src/features/projects/projectsSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { Project } from "../../types/project";
+
+type ProjectsState = {
+  projects: Project[];
+};
+
+const initialState: ProjectsState = {
+  projects: [],
+};
+
+const projectsSlice = createSlice({
+  name: "projects",
+  initialState,
+  reducers: {
+    setProjects: (state, action: PayloadAction<Project[]>) => {
+      state.projects = action.payload;
+    },
+
+    addProject: (state, action: PayloadAction<Project>) => {
+      state.projects.push(action.payload);
+    },
+
+    updateProject: (state, action: PayloadAction<Project>) => {
+      const index = state.projects.findIndex(
+        (project) => project.id === action.payload.id
+      );
+
+      if (index !== -1) {
+        state.projects[index] = action.payload;
+      }
+    },
+
+    deleteProject: (state, action: PayloadAction<string>) => {
+      state.projects = state.projects.filter(
+        (project) => project.id !== action.payload
+      );
+    },
+  },
+});
+
+export const {
+  setProjects,
+  addProject,
+  updateProject,
+  deleteProject,
+} = projectsSlice.actions;
+
+export default projectsSlice.reducer;

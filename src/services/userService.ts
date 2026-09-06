810d88f1de7b20b05cf4ee7b14c0402c4b168da8
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-07 00:59:08 +0330

    feat: add user api service

---
+++ src/services/userService.ts
@@ -0,0 +1,35 @@
+import api from "./api";
+import type { User } from "../types/user";
+
+export const getUsers = async (): Promise<User[]> => {
+  const response = await api.get<User[]>("/users");
+
+  return response.data;
+};
+
+export const getUserById = async (id: string): Promise<User> => {
+  const response = await api.get<User>(`/users/${id}`);
+
+  return response.data;
+};
+
+export const createUser = async (
+  user: Omit<User, "id">
+): Promise<User> => {
+  const response = await api.post<User>("/users", user);
+
+  return response.data;
+};
+
+export const updateUser = async (
+  id: string,
+  user: Omit<User, "id">
+): Promise<User> => {
+  const response = await api.put<User>(`/users/${id}`, user);
+
+  return response.data;
+};
+
+export const deleteUser = async (id: string): Promise<void> => {
+  await api.delete(`/users/${id}`);
+};

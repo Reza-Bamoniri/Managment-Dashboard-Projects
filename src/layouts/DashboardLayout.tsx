055e38ec9aae8a4180be91920d9f0786ca4bb294
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-07 16:52:39 +0330

    feat: connect sidebar to dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router";
+import Sidebar from "../components/Sidebar";
 
 function DashboardLayout() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <main className="min-h-screen">
+      <Sidebar />
+
+      <main className="min-h-screen pl-64">
         <Outlet />
       </main>
     </div>

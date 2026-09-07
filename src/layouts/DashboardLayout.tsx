8cc5bc7b3faffe3f07d8b262eebc3b01c044ad39
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-07 23:35:08 +0330

    style: add dark mode to dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -7,7 +7,7 @@ function DashboardLayout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-950">
       <Sidebar
         isOpen={isSidebarOpen}
         onClose={() => setIsSidebarOpen(false)}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,27 +8,20 @@ import RecentComments from "../components/home/RecentComments";
 
 function Home() {
   return (
-    <div
-      className="
-        min-h-full
-        space-y-6
-        bg-linear-to-br
-        from-green-50/70
-        via-white
-        to-lime-50/40
-        dark:from-gray-950
-        dark:via-gray-800
-        dark:to-green-950/40
-      "
-    >
+    <div className="min-h-full space-y-6 dark:bg-gray-950">
       <WelcomeSection />
+
       <SummaryCards />
+
       <TaskActivityChart />
+
       <ProjectsOverview />
+
       <RecentTasks />
 
       <section className="grid grid-cols-1 gap-6 lg:grid-cols-2">
         <UpcomingDeadlines />
+
         <RecentComments />
       </section>
     </div>

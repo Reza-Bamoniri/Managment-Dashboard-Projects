3bc972ecdd372a5c37378e939a42c384b8e4003f
Reza_Bamoniri <rezabri806@gmail.com>
2026-09-07 16:54:19 +0330

    feat: connect header to dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
 function DashboardLayout() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Sidebar />
 
-      <main className="min-h-screen pl-64">
-        <Outlet />
-      </main>
+      <div className="pl-64">
+        <Header />
+
+        <main className="min-h-[calc(100vh-4rem)] p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
